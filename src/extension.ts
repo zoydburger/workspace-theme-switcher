61372d8839af4fb6b12093ae67cb81224f6f8ed8
zoydburger <roman@salameh.de>
2024-06-28 12:58:51 +0200

    feature: implement basic theme switcher

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,103 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const LOGGING_PREFIX = '[workspace-theme-switcher]';
+const CONFIG_PREFIX = 'workspace-theme-switcher';
+const THEME_CONFIG_KEY = 'workbench.colorTheme';
+
+type Config = {
+  defaultTheme: string;
+  workspaces: Array<{ path: string; theme: string }>;
+};
+
 export function activate(context: vscode.ExtensionContext) {
+  log('info', 'Extension activated');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "workspace-theme-switcher" is now active!');
+  const rootPath = vscode.workspace.workspaceFolders
+    ? vscode.workspace.workspaceFolders[0].uri.fsPath
+    : undefined;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('workspace-theme-switcher.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from workspace-theme-switcher!');
-	});
+  if (rootPath === undefined) {
+    log('error', 'Error initializing extension: Could not determine root path');
+    return;
+  }
 
-	context.subscriptions.push(disposable);
+  const editor = vscode.window.activeTextEditor;
+  if (editor !== undefined) {
+    const filePath = editor.document.uri.fsPath;
+    const config = vscode.workspace.getConfiguration();
+    void setThemeForFile(rootPath, filePath, config);
+    vscode.window.showInformationMessage(
+      `${LOGGING_PREFIX} initialized with theme '${config.get(
+        THEME_CONFIG_KEY
+      )}'`
+    );
+  }
+
+  vscode.window.onDidChangeActiveTextEditor((editor) => {
+    if (editor) {
+      const filePath = editor.document.uri.fsPath;
+      const config = vscode.workspace.getConfiguration();
+      void setThemeForFile(rootPath, filePath, config);
+    }
+  });
+}
+
+function setThemeForFile(
+  rootPath: string,
+  filePath: string,
+  config: vscode.WorkspaceConfiguration
+): string | undefined {
+  const workspaces = config.get<Config['workspaces']>(
+    `${CONFIG_PREFIX}.workspaces`
+  );
+  if (workspaces === undefined || workspaces.length === 0) {
+    log('warn', 'No workspaces configured');
+    return;
+  }
+
+  workspaces.sort((a, b) => b.path.length - a.path.length);
+  const workspace = workspaces.find((workspace) =>
+    filePath.startsWith(path.join(rootPath, workspace.path))
+  );
+
+  const theme =
+    workspace?.theme ??
+    config.get<Config['defaultTheme']>(`${CONFIG_PREFIX}.defaultTheme`);
+
+  if (!theme) {
+    log('warn', 'No theme found for file');
+    return;
+  }
+
+  if (theme !== config.get(THEME_CONFIG_KEY)) {
+    config
+      .update(THEME_CONFIG_KEY, theme, vscode.ConfigurationTarget.Workspace)
+      .then(
+        () => {
+          log('info', `Updated theme to ${theme}`);
+        },
+        (error) => {
+          log('error', `Error updating theme: ${error}`);
+        }
+      );
+  }
+}
+
+function log(severity: 'error' | 'warn' | 'info', ...message: Array<any>) {
+  const output = `${LOGGING_PREFIX} ${message}`;
+  switch (severity) {
+    case 'error':
+      console.error(output);
+      break;
+    case 'warn':
+      console.warn(output);
+      break;
+    default:
+      console.log(output);
+  }
 }
 
 // This method is called when your extension is deactivated
